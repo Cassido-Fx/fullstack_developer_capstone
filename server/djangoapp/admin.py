aa6576d257fec31770af761c3798fb773004bc3f
cassido-fx <okaforcassy@gmail.com>
2024-10-15 10:28:14 -0400

    CarModel and CarMake models added

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,4 @@
-# from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
@@ -11,3 +10,27 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+# CarModelInline class: Inline admin descriptor for CarModel objects
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Number of empty CarModel objects displayed by default in the admin
+
+# CarModelAdmin class: Custom admin class for CarModel
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'car_make', 'car_type', 'year']  # Fields to display in the admin list view
+    list_filter = ['car_make', 'car_type', 'year']  # Add filters to the right sidebar
+    search_fields = ['name', 'car_make__name']  # Enable search by car model name or car make name
+
+# CarMakeAdmin class: Custom admin class for CarMake, with CarModel inline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']  # Fields to display in the admin list view
+    search_fields = ['name']  # Enable search by car make name
+    inlines = [CarModelInline]  # Attach the CarModel inline
+
+# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path(route='logout', view=views.logout, name='logout'),
     # path for registration
     path(route='register/', view=views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
